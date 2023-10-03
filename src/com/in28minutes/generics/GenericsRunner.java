52f90bd8956b29aae1d12f6d7650a0837bad0286
TaeMin Kim <gimtaemin36@gmail.com>
2023-10-04 00:10:26 +0900

    제네릭의 상한 경계와 하한 경계

--- src/com/in28minutes/generics/GenericsRunner.java
+++ src/com/in28minutes/generics/GenericsRunner.java
@@ -5,10 +5,33 @@ import java.util.List;
 
 public class GenericsRunner {
 
+    static double sumOfNumberList(List<? extends Number> numbers) { // 상한 경계
+        double sum = 0.0;
+        for (Number number : numbers) {
+            sum += number.doubleValue();
+        }
+        return sum;
+    }
+
+    static void addACoupleOfValues(List<? super Number> numbers) { // 하한 경
+        numbers.add(1);
+        numbers.add(1.0);
+        numbers.add(1L);
+        numbers.add(1.0f);
+    }
+
     static <X extends List> void duplicate(X list){
         list.addAll(list);
     }
     public static void main(String[] args) {
+
+        System.out.println(sumOfNumberList(List.of(1,2,3,4,5)));
+        System.out.println(sumOfNumberList(List.of(1.1,2.1,3.1,4.1,5.1)));
+        System.out.println(sumOfNumberList(List.of(1L,2L,3L,4L,5L)));
+
+        List emptyList = new ArrayList<Number>();
+        addACoupleOfValues(emptyList);
+        System.out.println(emptyList);
 
         ArrayList<Integer> numbers = new ArrayList<>(List.of(1, 2, 3));
         duplicate(numbers);
