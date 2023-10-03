f16be10cdd257c46b0544e21e79f4f91221fb1db
TaeMin Kim <gimtaemin36@gmail.com>
2023-10-03 23:52:35 +0900

    제네릭의 제한점과 확장

--- src/com/in28minutes/generics/GenericsRunner.java
+++ src/com/in28minutes/generics/GenericsRunner.java
@@ -1,7 +1,19 @@
 package com.in28minutes.generics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GenericsRunner {
+
+    static <X extends List> void duplicate(X list){
+        list.addAll(list);
+    }
     public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>(List.of(1, 2, 3));
+        duplicate(numbers);
+        System.out.println(numbers);
+
         MyCustomList<String> list = new MyCustomList<>();
         list.addElement("Element 1");
         list.addElement("Element 2");
