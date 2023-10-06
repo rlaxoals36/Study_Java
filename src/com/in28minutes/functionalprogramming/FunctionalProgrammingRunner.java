477a0e60b5f7b6266cbf804e22a6c54ab7714deb
TaeMin Kim <gimtaemin36@gmail.com>
2023-10-07 07:02:04 +0900

    함수형 프로그래밍 - 필터

--- src/com/in28minutes/functionalprogramming/FunctionalProgrammingRunner.java
+++ src/com/in28minutes/functionalprogramming/FunctionalProgrammingRunner.java
@@ -4,9 +4,9 @@ import java.util.List;
 
 public class FunctionalProgrammingRunner {
     public static void main(String[] args) {
-        List<String> list = List.of("Apple","Banana","Cat","Dog");
+        List<String> list = List.of("Apple","Bat","Cat","Dog");
 
-        printBasic(list);
+        printWithFPWithFilter(list);
     }
 
     private static void printBasic(List<String> list) {
@@ -15,10 +15,23 @@ public class FunctionalProgrammingRunner {
         }
     }
 
+    private static void printBasicWithFilter(List<String> list) {
+        for (String string : list) {
+            if (string.endsWith("at"));
+                System.out.println(string);
+        }
+    }
+
     private static void printWithFP(List<String> list) {
         list.stream().forEach(
                 element -> System.out.println(element)   // 람다식
         );
+    }
+
+    private static void printWithFPWithFilter(List<String> list) {
+        list.stream()
+                .filter(element -> element.endsWith("at"))
+                .forEach(element -> System.out.println(element));   // 람다식
     }
 
 }
