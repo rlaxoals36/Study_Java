b82c04fbf344671707707561c59a9e84c6549d45
TaeMin Kim <gimtaemin36@gmail.com>
2023-10-07 17:08:29 +0900

    함수형 인터페이스 이면의 원리 - Function 구현

--- src/com/in28minutes/functionalprogramming/LambdaBehindTheScreensRunner.java
+++ src/com/in28minutes/functionalprogramming/LambdaBehindTheScreensRunner.java
@@ -2,6 +2,7 @@ package com.in28minutes.functionalprogramming;
 
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 class EvenNumberPredicate implements Predicate<Integer> {
@@ -10,21 +11,28 @@ class EvenNumberPredicate implements Predicate<Integer> {
         return number%2==0;
     }
 }
-
 class SystemOutConsumer implements Consumer<Integer> {
     @Override
     public void accept(Integer integer) {
         System.out.println(integer);
     }
 }
+class NumberSquareMapper implements Function<Integer,Integer> {
+    @Override
+    public Integer apply(Integer integer) {
+        return integer*integer;
+    }
+}
 public class LambdaBehindTheScreensRunner {
     public static void main(String[] args) {
         List.of(34,25,45,36).stream()
                 .filter(n->n%2==0)
+                .map(n -> n*n)
                 .forEach(e->System.out.println(e));
 
         List.of(34,25,45,36).stream()
                 .filter(new EvenNumberPredicate())
+                .map(new NumberSquareMapper())
                 .forEach(new SystemOutConsumer());
 
         // .filter()
@@ -34,5 +42,9 @@ public class LambdaBehindTheScreensRunner {
         // .forEach()
         // void forEach(Consumer<? super T> action);
         //  void accept(T t);
+
+        // .map()
+        // <R> Stream<R> map(Function<? super T, ? extends R> mapper);
+        // R apply(T t);
     }
 }
